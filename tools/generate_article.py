563b24edfd4d22c2d64428908a168c4f336c255b
Andrii <14119286+w1ne@users.noreply.github.com>
2023-07-28 15:46:54 +0200

    28.07.23

--- tools/generate_article.py
+++ tools/generate_article.py
@@ -17,16 +17,21 @@ def generate_description(event):
     openai.api_key = os.getenv("OPENAI_BUDAPEST_INK_API_KEY")
     messages = [
         {"role": "system", "content": "I Want You To Act As A Event Explorer. Very Proficient SEO in Fluently English. Write in Your Own Words Rather Than Copying And Pasting From Other Sources. Consider perplexity and burstiness when creating content, ensuring high levels of both without losing specificity or context. Use detailed paragraphs that engage the reader. Write In A Conversational Style As Written By A Human (Use An Informal Tone, Utilize Personal Pronouns, Keep It Simple, Engage The Reader, Use The Active Voice, Keep It Brief, Use Rhetorical Questions, and Incorporate Analogies And Metaphors). Put short description of the event so people know where, when what. Format in MD. The article should be ready to publish as event announcment.:\n"},
-	    {"role": "user", "content": f"Here are the details of the event:\n\nName: {event['name']}\nDate and Time: {event['date_time']}\nLocation: {event['address']}\nOrganizer: {event['organizer_name']}"}
+	    {"role": "user", "content": f"Here are the details of the event:\n\nName: {event['name']}\nDate and Time: {event['date_time']}\nLocation: {event['address']}\nOrganizer: {event['organizer_name']}\nText: {event['text']}"}
     ]
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages,
-        max_tokens=1000
-    )
-
-    return response['choices'][0]['message']['content'].strip()
+    for i in range(3):  # Retry up to 3 times
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages,
+                max_tokens=1000
+            )
+            return response['choices'][0]['message']['content'].strip()
+        except openai.error.APIError as e:
+                print(f"OpenAI API request failed for event '{event['name']}' on attempt {i+1}: {e}")
+                time.sleep(5)  # Wait for 5 seconds before retrying
+    return None  # Return None if all retries failed
 
 def process_events_from_file(file_path):
     with open(file_path, 'r') as f:
@@ -49,7 +54,7 @@ def main():
     parser.add_argument("file_path", help="Path to the JSON file containing events")
     args = parser.parse_args()
 
-    top_5_events = process_events_from_file(args.file_path)
+    top_ten_events = process_events_from_file(args.file_path)
 
     # Create a new markdown file with today's date
     filename = datetime.now().strftime("%Y-%m-%d") + "-events.md"
@@ -63,13 +68,16 @@ def main():
         f.write("draft: false\n")
         f.write("---\n\n")
 
-        for event in top_5_events:
+        for event in top_ten_events:
             name = event.get('name')
             if name is None:
                 print(f"Missing 'name' in event: {event}")
                 name = "Untitled Event"
             
             description = generate_description(event)
+            if description is None:
+                print(f"Failed to generate description for event: {event}")
+                continue
             f.write(f"![Event Image]({event['image']})\n\n ### {name}\n\n{description}\n[Event Link](https://facebook.com{event['url']})\n")
             f.write("\n") 
             f.write("---") 
