9adfced83d5d4f9dae964b0347fdcbd19ca95f91
Andrii <14119286+w1ne@users.noreply.github.com>
2023-07-31 21:17:58 +0200

    added HU

--- tools/generate_article.py
+++ tools/generate_article.py
@@ -12,13 +12,19 @@ def extract_number(s):
 
     match = re.search(r'\d+', s)
     return int(match.group()) if match else 0
-
-def generate_description(event):
+    
+def generate_description(event, lang):
     openai.api_key = os.getenv("OPENAI_BUDAPEST_INK_API_KEY")
+    system_prompt = ""
+    # Use English or Hungarian prompts based on `lang`
+    if lang == "en":
+       system_prompt = 'Act As A Event Explorer. Very Proficient SEO in Fluently English. Write in Your Own Words Rather Than Copy From Other Sources. Consider perplexity and burstiness when creating content, ensuring high levels of both without losing specificity or context. Use detailed paragraphs that engage the reader. Write In A Conversational Style As Written By A Human (Use An Informal Tone, Utilize Personal Pronouns, Keep It Simple, Engage The Reader, Use The Active Voice, Keep It Brief, Use Rhetorical Questions, and Incorporate Analogies And Metaphors). Put short description of the event so people know where, when, what. Format in MD. If you do not know something, do not put explanations or empty links. For location in coordinates paste maps link. Time is Budapest local time. The article should be ready to publish as event announcment'
+    else:  # Assume Hungarian
+       system_prompt = 'Act As A Event Explorer. Very Proficient SEO in Fluently Hungarian. Write in Hungarian in Your Own Words. Consider perplexity and burstiness when creating content, ensuring high levels of both without losing specificity or context. Use detailed paragraphs that engage the reader. Write In A Conversational Style As Written By A Human (Use An Informal Tone, Utilize Personal Pronouns, Keep It Simple, Engage The Reader, Use The Active Voice, Keep It Brief, Use Rhetorical Questions, and Incorporate Analogies And Metaphors). Put short description of the event so people know where, when, what. Format in MD. If you do not know something, do not put explanations or empty links. For location in coordinates paste maps link. Time is Budapest local. The article should be ready to publish as event announcment'
     messages = [
-        {"role": "system", "content": "Act As A Event Explorer. Very Proficient SEO in Fluently English. Write in Your Own Words Rather Than Copy From Other Sources. Consider perplexity and burstiness when creating content, ensuring high levels of both without losing specificity or context. Use detailed paragraphs that engage the reader. Write In A Conversational Style As Written By A Human (Use An Informal Tone, Utilize Personal Pronouns, Keep It Simple, Engage The Reader, Use The Active Voice, Keep It Brief, Use Rhetorical Questions, and Incorporate Analogies And Metaphors). Put short description of the event so people know where, when, what. Format in MD. If you do not know something, do not put explanations or empty links. For location in coordinates paste maps link. Time is Budapest local time. The article should be ready to publish as event announcment.:\n"},
+	    {"role": "system", "content": system_prompt},
 	    {"role": "user", "content": f"Details of the event:\n\nName: {event['name']}\nDate and Time: {event['date_time']}\nLocation: {event['address']}\nOrganizer: {event['organizer_name']}\nText: {event['text']}"}
-    ]
+	]
 
     for i in range(3):  # Retry up to 3 times
         try:
@@ -56,32 +62,37 @@ def main():
 
     top_ten_events = process_events_from_file(args.file_path)
 
-    # Create a new markdown file with today's date
-    filename = datetime.now().strftime("%Y-%m-%d") + "-events.md"
-    filepath = os.path.join("..", "content", "posts", filename)  # adjust the path according to your Hugo project's structure
+    # Generate posts for both languages
+    for lang in ["en", "hu"]:
+        # Create a new markdown file with today's date
+        filename = datetime.now().strftime("%Y-%m-%d") + "-events." + ".md"
+        filepath = os.path.join("..", "content", lang, "posts", filename)  # Adjust path according to your Hugo project's structure
 
-    with open(filepath, "w") as f:
-        # Write the required Hugo front matter
-        f.write("---\n")
-        f.write("title: Top 10 events for today in Budapest\n")
-        f.write("date: " + datetime.now().strftime("%Y-%m-%d") + "\n")
-        f.write("draft: false\n")
-        f.write("---\n\n")
+        with open(filepath, "w") as f:
+            # Write the required Hugo front matter
+            f.write("---\n")
+            if lang == "en":
+                f.write("title: Top 10 events for today in Budapest\n")
+            else:
+                f.write("title: Top 10 esemény ma Budapesten\n")
+            f.write("date: " + datetime.now().strftime("%Y-%m-%d") + "\n")
+            f.write("draft: false\n")
+            f.write("---\n\n")
 
-        for event in top_ten_events:
-            name = event.get('name')
-            if name is None:
-                print(f"Missing 'name' in event: {event}")
-                name = "Untitled Event"
-            
-            description = generate_description(event)
-            if description is None:
-                print(f"Failed to generate description for event: {event}")
-                continue
-            f.write(f"![Event Image]({event['image']})\n\n ### {name}\n\n{description}\n[Event Link](https://facebook.com{event['url']})\n")
-            f.write("\n") 
-            f.write("---") 
-            f.write("\n") 
+            for event in top_ten_events:
+                name = event.get('name')
+                if name is None:
+                    print(f"Missing 'name' in event: {event}")
+                    name = "Untitled Event"  # This should be translated for Hungarian
+                
+                description = generate_description(event, lang)
+                if description is None:
+                    print(f"Failed to generate description for event: {event}")
+                    continue
+                f.write(f"![Event Image]({event['image']})\n\n ### {name}\n\n{description}\n[Event Link](https://facebook.com{event['url']})\n")
+                f.write("\n") 
+                f.write("---") 
+                f.write("\n") 
 
 if __name__ == "__main__":
     main()
